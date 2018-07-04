b75d9bfb068d0e247270ada2049dfd57ef4f8368
zjx-immersion <jianxin_home@163.com>
2018-07-04 16:33:52 +0800

    [Jason] upgrade the jasmine version and add the test report in cli

--- run-tests.js
+++ run-tests.js
@@ -3,4 +3,20 @@ const jasmine = new Jasmine();
 
 jasmine.loadConfigFile('spec/support/jasmine.json');
 
+// setup console reporter
+const JasmineConsoleReporter = require('jasmine-console-reporter');
+const reporter = new JasmineConsoleReporter({
+    colors: 1,           // (0|false)|(1|true)|2
+    cleanStack: 1,       // (0|false)|(1|true)|2|3
+    verbosity: 4,        // (0|false)|1|2|(3|true)|4
+    listStyle: 'indent', // "flat"|"indent"
+    activity: true,
+    emoji: true,         // boolean or emoji-map object
+    beep: true
+});
+ 
+// initialize and execute
+jasmine.env.clearReporters();
+jasmine.addReporter(reporter);
+
 jasmine.execute();
